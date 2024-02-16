3aec6d09df05819030b8994a3f9e5cc11d5ca9d8
reed <reed@pogi.com>
2024-02-16 10:52:39 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Coin from "./components/Coin";
 
 function App() {
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&order=market_cap_desc&sparkline=false";
+  const [cryptoList, setCryptoList] = useState([]);
+  const [searchCoin, setSeacrhCoin] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setCryptoList(res.data);
+      })
+      .catch((err) => {
+        console.error("Can't fetch api data.", err);
+      });
+  }, []);
+
+  const coins = cryptoList.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchCoin.toLowerCase());
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cryptoHeader">
+        <input
+          type="text"
+          placeholder="Enter coin name..."
+          onChange={(event) => setSeacrhCoin(event.target.value)}
+        />
+      </div>
+
+      <div className="cryptoDisplay">
+        {coins.map((coin) => {
+          return (
+            <Coin
+              name={coin.name}
+              icon={coin.image}
+              price={coin.current_price}
+              symbol={coin.symbol}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
